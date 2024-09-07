b7d4d2c96257e96a900387598661fe0e2cf48b37
Himanshu21135 <himanshu2022@iisc.ac.in>
2024-09-07 17:44:40 +0530

    update

--- code/api.py
+++ code/api.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from inference import model_fn, input_fn, predict_fn, output_fn
 from pydantic import BaseModel
 import sys
-    sys.path.append('code')
+
 import base64
 from w2l_decoder import  infer
 class UserCreate(BaseModel):
@@ -23,5 +23,5 @@ async def read_root(data: UserCreate):
     
     # encode_string = base64.b64encode(open("1.mp3", "rb").read()).decode()
     
-    out = infer(data,'/mnt/External/8TBHDD/Keyur/IndicWav2vecHindiInference/code/config.yaml')
+    out = infer(data,'config.yaml')
     return out
--- code/w2l_decoder.py
+++ code/w2l_decoder.py
@@ -1,5 +1,13 @@
 import sys
-sys.path.append('/mnt/External/8TBHDD/Keyur/IndicWav2Vec/fairseq')
+import os
+from pathlib import Path
+cwd = os.path.realpath('.')
+
+relative_path = "code/fairseq"
+full_path = os.path.join(cwd, relative_path)
+
+print(full_path)
+sys.path.append(full_path)
 from examples.speech_recognition.w2l_decoder import W2lViterbiDecoder
 
 import yaml
